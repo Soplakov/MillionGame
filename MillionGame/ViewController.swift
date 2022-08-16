68605c94fff16747661da76d6c6bf0c8cd7f0225
Soplakov <soplakov@gmail.com>
2022-08-16 23:30:11 +0500

    Initial Commit

---
+++ MillionGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionGame
+//
+//  Created by PiZero on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
