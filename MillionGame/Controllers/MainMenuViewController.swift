b3b7b6e86202819c7577010127a2fa72aa198630
Soplakov <soplakov@gmail.com>
2022-08-22 15:24:00 +0500

    Создание UIView главного меню игры

---
+++ MillionGame/Controllers/MainMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainMenuViewController.swift
+//  MillionGame
+//
+//  Created by PiZero on 22.08.2022.
+//
+
+import UIKit
+// Экран главного меню игры
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
